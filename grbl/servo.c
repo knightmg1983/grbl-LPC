df2b868fd4cbfdf418403f26027ddf560dd89874
knightmg1983 <35811785+knightmg1983@users.noreply.github.com>
2021-02-22 22:43:54 -0500

    Update servo.c

--- grbl/servo.c
+++ grbl/servo.c
@@ -1,1 +1,17 @@
+#include "grbl.h"
 
+
+void servo_init()
+{
+  #ifdef HAS_SERVOS
+  //initialize servo #1
+  
+  #endif
+  /*
+  COOLANT_FLOOD_DDR |= (1 << COOLANT_FLOOD_BIT); // Configure as output pin
+  #ifdef ENABLE_M7
+    COOLANT_MIST_DDR |= (1 << COOLANT_MIST_BIT);
+  #endif
+  coolant_stop();
+  */
+}
